37fee8d881b692191cb01d1b8e5d96153126fbac
Mamun Ar Rashid <mamuncseruet03@gmail.com>
2024-07-17 18:19:00 +0600

    added env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -47,14 +47,10 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+            {" "}
+            mmmmmmmm {process.env.DB_HOST}
           </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
+          <p className="m-0 max-w-[30ch] text-sm opacity-50"></p>
         </a>
 
         <a
